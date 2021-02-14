0f37acd363181b157ac9e11a05af6d812b119dc2
David Broman <dbro@kth.se>
2021-02-14 18:53:22 +0100

    Minor fix in the text

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -6,11 +6,22 @@ const foo = (x: number) => {
 }
 
 const Home = () => {
-  return <div className="container"><h1>Miking</h1>The official Miking website will appear in year {foo(2000)} and 
-  an official beta release of Miking is planned to March 2021.
-  <p>For a quick peek into the current development, you can take a look at the 
-    <a href="https://github.com/miking-lang/miking/tree/develop"> development branch on Github</a>. 
-    </p></div>
+  return (
+    <div className="container">
+      <h1>Miking</h1>The official Miking website will appear later this year (
+      {foo(2020)}) and an official beta release of Miking is planned to March
+      2021.
+      <p>
+        For a quick peek into the current development, you can take a look at
+        the
+        <a href="https://github.com/miking-lang/miking/tree/develop">
+          {" "}
+          development branch on Github
+        </a>
+        .
+      </p>
+    </div>
+  )
 }
 
 export default Home
